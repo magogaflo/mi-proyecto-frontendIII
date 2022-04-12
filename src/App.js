d692df42d20f68b18e977f61127bb6d8ed757583
magogaflo <patt8i@hotmail.com>
2022-04-11 22:26:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ListaPreguntas from "./component/ListaPreguntas";
+
+import "./App.css";
+
+
+class App extends React.Component {
+ 
+  render() {
+    return (
+      <div className="App">
+      
+        <ListaPreguntas />
+       
+      </div>
+    );
+  }
+}
+export default App;
